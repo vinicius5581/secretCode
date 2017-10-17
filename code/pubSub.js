bab06d6e1b77d728631ffa1f118537c53e25e43d
Vinicius Santana <santanaviny@gmail.com>
2017-10-17 02:32:27 -0700

    Publish subscribe pathern

---
+++ code/pubSub.js
@@ -0,0 +1,31 @@
+function PubSub() {
+	var hashData = {};
+
+	function publish (event, payload) {
+		hashData[event].map(callback => {
+			callback(payload)
+		});
+	}
+
+	function subscribe (event, callback) {
+		if (!hashData[event]){
+			hashData[event] = [];
+		}
+		hashData[event].push(callback);
+	}
+
+	return {
+		publish: publish,
+		subscribe: subscribe
+	}
+}
+
+
+var publishSubcriber = new PubSub();
+
+publishSubcriber.subscribe('event', (data) => console.log(data));
+publishSubcriber.subscribe('event', (data) => console.log(data + 1));
+
+window.setTimeout(() => {
+	publishSubcriber.publish('event', 2);
+}, 2000);
