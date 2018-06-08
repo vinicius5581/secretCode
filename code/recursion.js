1d4f600b3225e1631039f62d9d315e4570c31f7b
Vinicius Santana <santanaviny@gmail.com>
2018-06-07 22:43:30 -0700

    Update recursion.js

--- code/recursion.js
+++ code/recursion.js
@@ -1,14 +1,7 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const add = (x,y) => x + y;
-const sum = ([x, y,  ...ys]) => {
-    if (x === undefined) {
-        return 0;
-    } else if (y === undefined) {
-        return x;
-    } else if (ys.length === 0) {
-      return add(x, y);
-    } else {
-      return add(x, sum([y, ...ys]));
-    }
-};
+const sum = ([x, y,  ...ys]) => x === undefined ? 0
+    : y === undefined ? 0
+    : ys.length === 0 ? add(x,y)
+    : add(x, sum([y, ...ys]));
 const result = sum(nums);
