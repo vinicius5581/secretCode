df10a944a8e10c6507c2a7cf4c3e9ded16b81b8e
Vinicius Santana <santanaviny@gmail.com>
2018-06-07 22:50:16 -0700

    Update recursion.js

--- code/recursion.js
+++ code/recursion.js
@@ -5,3 +5,14 @@ const sum = ([x, y,  ...ys]) => x === undefined ? 0
     : ys.length === 0 ? add(x,y)
     : add(x, sum([y, ...ys]));
 const result = sum(nums);
+
+
+const words =  ['I', 'know', 'recursion'];
+const joinWithSpace = (x,y) => `${x} ${y}`;
+const join = ([x, y,  ...ys]) => x === undefined ? ''
+    : y === undefined ? x
+    : ys.length === 0 ? joinWithSpace(x,y)
+    : joinWithSpace(x, join([y, ...ys]));
+
+
+const sentence = join(words);
